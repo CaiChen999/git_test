2342b6b6714b5c5c2a23f884e16e65ca19859c93
SPECTER <1792572101@qq.com>
2023-03-29 21:39:28 +0800

    first commit

---
+++ src/main/java/com/caichen/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.caichen.git;
+
+/**
+ * git仓库地址：
+ * 北海：https://gitee.com/cskaoyan-java/cskaoyan-2022-37th
+ * stone:https://gitee.com/stone4j/java37th
+ *
+ * @author ncc on 2023/3/29 21:31
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Git: hello 靓仔！");
+    }
+}
