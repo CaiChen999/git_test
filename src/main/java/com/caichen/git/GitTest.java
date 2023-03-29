0979fc6f3fcda198312ae86d662c963ef91018f6
SPECTER <1792572101@qq.com>
2023-03-29 21:42:30 +0800

    third commit

--- src/main/java/com/caichen/git/GitTest.java
+++ src/main/java/com/caichen/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git: hello 靓仔！");
         System.out.println("Git: hello 靓仔2！");
+        System.out.println("Git: hello 靓仔3！");
     }
 }
